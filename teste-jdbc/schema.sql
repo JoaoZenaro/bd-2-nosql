0a5e313895a563567428820dca94f27c5cf94708
João Marcelo Zenaro <joaomarcelo661@gmail.com>
2023-10-31 23:29:33 +0000

    feat: add schema.sql

---
+++ teste-jdbc/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE unoesc_crud_jdbc;
+USE unoesc_crud_jdbc;
+
+CREATE TABLE produto (
+    id_prod INT AUTO_INCREMENT PRIMARY KEY,
+    nome_prod VARCHAR(50),
+    data_cadastro DATE,
+    quantidade INT,
+    preco DECIMAL(10, 2)
+) engine=InnoDB;
+
+INSERT INTO produto
+VALUES (1, 'Notebook', '2023-01-01', 5, 666),
+    (2, 'Smartphone', '2023-10-30', 15, 1234.56),
+    (3, 'TV', '2023-04-01', 2, 4999.99),
+    (4, 'Smartband', '2023-04-01', 50, 499.99);
--- teste-jdbc/src/main/java/br/edu/unoesc/testejdbc/TesteJdbcApplication.java
+++ teste-jdbc/src/main/java/br/edu/unoesc/testejdbc/TesteJdbcApplication.java
@@ -1,13 +1,9 @@
 package br.edu.unoesc.testejdbc;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TesteJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TesteJdbcApplication.class, args);
+		System.out.println("Henlo");
 	}
 
 }
