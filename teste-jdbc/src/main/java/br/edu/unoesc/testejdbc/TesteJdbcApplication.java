c463b52cf4911289a28a01e52489cf07df664a78
João Marcelo Zenaro <joaomarcelo661@gmail.com>
2023-10-31 20:11:07 -0300

    fix: file organization

---
+++ teste-jdbc/src/main/java/br/edu/unoesc/testejdbc/TesteJdbcApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.testejdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJdbcApplication.class, args);
+	}
+
+}
---
+++ teste-jdbc/src/test/java/br/edu/unoesc/testejdbc/TesteJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.testejdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
